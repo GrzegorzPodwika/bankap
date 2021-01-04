89ffdaa1f2159942fc5763c8cfb6591af72233ea
Grzegorz <podwikagrzegorz@gmail.com>
2021-01-04 12:22:46 +0100

    Add transaction statement generation as pdf.

--- src/main/java/com/bank/application/ui/views/payments/PaymentsView.java
+++ src/main/java/com/bank/application/ui/views/payments/PaymentsView.java
@@ -9,29 +9,45 @@ import com.bank.application.backend.service.UserService;
 import com.bank.application.other.Constants;
 import com.bank.application.ui.views.home.HomeView.UserNotFoundException;
 import com.bank.application.ui.views.main.MainView;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.server.VaadinSession;
 import org.vaadin.crudui.crud.CrudOperation;
 import org.vaadin.crudui.crud.impl.GridCrud;
+import org.vaadin.olli.FileDownloadWrapper;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Route(value = "payments", layout = MainView.class)
 @PageTitle("Payments")
 public class PaymentsView extends VerticalLayout {
 
+    private byte[] pdfByteArray;
     private User activeUser;
     private final AccountService accountService;
     private final UserService userService;
+    private final TransactionService transactionService;
+    private List<Transaction> allTransactionsByUser;
 
 
     public PaymentsView(TransactionService transactionService, AccountService accountService, UserService userService) {
         setId("payments-view");
         this.userService = userService;
         this.accountService = accountService;
+        this.transactionService = transactionService;
 
         fetchActiveUser();
 
@@ -45,6 +61,7 @@ public class PaymentsView extends VerticalLayout {
                 "transactionTitle", "amount", "receiverAccountNumber");
 
 
+        allTransactionsByUser = transactionService.findAllByAccount(activeUser.getAccount());
         crud.setFindAllOperation(() -> transactionService.findAllByAccount(activeUser.getAccount()));
 
         crud.setAddOperation(transaction -> {
@@ -72,6 +89,8 @@ public class PaymentsView extends VerticalLayout {
                     updateBalanceInSenderAndReceiverAccounts(senderAccount, receiverAccount, transferAmountPositive);
                     transactionService.add(transaction, receiverTransaction);
 
+                    fetchTransactions();
+
                     Notification.show("Pomyślnie wysłano przelew!", 3000, Notification.Position.MIDDLE);
                     return transaction;
                 } else {
@@ -90,8 +109,30 @@ public class PaymentsView extends VerticalLayout {
         crud.setDeleteOperation(transactionService::delete);
 
         setSizeFull();
+
+        Button buttonGenerateStatement = new Button("Generuj wyciąg");
+        FileDownloadWrapper buttonWrapper = new FileDownloadWrapper(
+                new StreamResource("statement.pdf", () -> {
+                    try {
+                        generateAndExposeStatement();
+                    } catch (FileNotFoundException | DocumentException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        return new FileInputStream("statement.pdf");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }));
+        buttonWrapper.wrapComponent(buttonGenerateStatement);
+        add(buttonWrapper);
+
+
         add(crud);
     }
+
 
     private void fetchActiveUser() {
         try {
@@ -108,6 +149,50 @@ public class PaymentsView extends VerticalLayout {
             activeUser = fetchedUpdatedUser.get();
         } else {
             throw new UserNotFoundException();
+        }
+    }
+
+    private void fetchTransactions() {
+        allTransactionsByUser = transactionService.findAllByAccount(activeUser.getAccount());
+    }
+
+    private void generateAndExposeStatement() throws FileNotFoundException, DocumentException {
+        Document document = new Document();
+
+        PdfWriter.getInstance(document, new FileOutputStream("statement.pdf"));
+        document.open();
+
+/*        Font font = FontFactory.getFont(FontFactory.COURIER, 12, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+        document.add(chunk);*/
+
+        PdfPTable pdfPTable = new PdfPTable(5);
+        pdfPTable.setWidthPercentage(100f);
+        addTableHeader(pdfPTable);
+        addRows(pdfPTable);
+
+        document.add(pdfPTable);
+        document.close();
+    }
+
+    private void addTableHeader(PdfPTable pdfPTable) {
+        Stream.of("Konto", "Konto odbiorcy", "Tytuł przelewu", "Kwota", "Data")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2f);
+                    header.setPhrase(new Phrase(columnTitle));
+                    pdfPTable.addCell(header);
+                });
+    }
+
+    private void addRows(PdfPTable pdfPTable) {
+        for (Transaction trans: allTransactionsByUser) {
+            pdfPTable.addCell(trans.getAccount().getAccountNumber());
+            pdfPTable.addCell(trans.getReceiverAccountNumber());
+            pdfPTable.addCell(trans.getTransactionTitle());
+            pdfPTable.addCell(String.valueOf(trans.getAmount()));
+            pdfPTable.addCell(trans.getDate());
         }
     }
 
